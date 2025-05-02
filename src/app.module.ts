18fd271ce7e68584f8d458c80db7f48a1fd78d57
Web developer <150703003+mirkamolhomidov@users.noreply.github.com>
2025-05-02 12:36:44 +0500

    yangi fayllar qo'shildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [PrismaModule, PostsModule],
+  imports: [PrismaModule, PostsModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
